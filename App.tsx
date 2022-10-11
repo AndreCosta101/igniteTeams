de10188af1ac282cd94858c2dc85b2aaa1e2824c
AndreCosta <andre.costa101@gmail.com>
2022-10-11 16:21:40 -0300

    chore(style): add fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
-import Groups from './src/screens/Groups';
-import theme from './src/theme';
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
+import Groups from './src/screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups/>
+      { fontsLoaded ? <Groups/> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
