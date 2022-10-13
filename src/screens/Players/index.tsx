ee3ba53febab4c8a015119997ed4019cbccdf90c
AndreCosta <andre.costa101@gmail.com>
2022-10-13 14:22:25 -0300

    refactor(): variable, components' names

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { FlatList } from 'react-native';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Header } from '../../components/Header';
 import { Filter } from '../../components/Filter';
 import { HighLight } from '../../components/HighLight';
 import { Input } from '../../components/Input';
-import { Container, Form, HeaderList, PlayersNumber } from './styles';
+import { PlayerCard } from '../../components/PlayerCard';
+import { EmptyList } from '../../components/EmptyList';
+import { Button } from '../../components/Button';
+import { Container, Form, HeaderList, PlayersQuantity } from './styles';
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
@@ -42,10 +45,37 @@ export function Players() {
             )}
             horizontal={true}
           />
-          <PlayersNumber>
+          <PlayersQuantity>
               {players.length}
-          </PlayersNumber>
+          </PlayersQuantity>
         </HeaderList> 
+
+        <FlatList
+          data={players}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <PlayerCard 
+              name={item}
+              onRemove={() => {}} 
+            />
+          )}
+          ListEmptyComponent={() => (
+            <EmptyList
+              message="Não há pessoas nesse time"
+            />
+          )}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={[
+            { paddingBottom: 100 },
+            players.length === 0 && { flex: 1 }
+          ]}     
+        />
+
+        <Button
+          title="Remover Turma"
+          type='SECONDARY'
+          onPress={() => {}}
+        />
 
     </Container>
   );
