cf46a65a7c1f94f206a02577de90bd18edceef9d
AndreCosta <andre.costa101@gmail.com>
2022-10-14 11:07:28 -0300

    refactor(style): implement  safe area view

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,24 +4,34 @@ import { HighLight } from "../../components/HighLight";
 import { Input } from "../../components/Input";
 import { Container, Content, Icon } from "./styles";
 
+import { useNavigation } from "@react-navigation/native";
+
 export function NewGroup() {
-  return (
-    <Container>
-        <Header showBackButton />
-        <Content>
-            <Icon/>
-            <HighLight
-                title="Nova turma"
-                subTitle="crie a turma para adicionar as pessoas"
-            />
-            <Input 
-                placeholder="Nome da turma"
-            />
-            <Button 
-                title="Criar"
-                style={{marginTop: 20}}
-            />
-        </Content>
-    </Container>
-  );
+    const navigation = useNavigation();
+
+    function navigateToPlayers(){
+        navigation.navigate('players', { group: 'Rocket'});
+    }
+
+    
+    return (
+        <Container>
+            <Header showBackButton />
+            <Content>
+                <Icon/>
+                <HighLight
+                    title="Nova turma"
+                    subTitle="crie a turma para adicionar as pessoas"
+                />
+                <Input 
+                    placeholder="Nome da turma"
+                />
+                <Button 
+                    title="Criar"
+                    style={{marginTop: 20}}
+                    onPress={navigateToPlayers}
+                />
+            </Content>
+        </Container>
+    );
 }
